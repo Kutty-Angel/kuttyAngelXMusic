4c87da87e5f41cdffcbd63944427aa316fcc9a7f
Kutty-Angel <107125414+Kutty-Angel@users.noreply.github.com>
2022-06-13 16:34:18 +0530

    Update pmmsg.py

--- m8n/modules/pmmsg.py
+++ m8n/modules/pmmsg.py
@@ -11,5 +11,5 @@ from m8n.config import (
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Hey 👋 I am the assistant of music bot, didn't have a time to talk with you 🙂 kindly join @M8N_SUPPORT for getting support\n\nPowered by @M8N_OFFICIAL")
+  await USER.send_message(message.chat.id,"Hey 👋 I am the assistant of music bot, didn't have a time to talk with you 🙂 kindly join @M8N_SUPPORT for getting support\n\nPowered by @DuskyBotZUpdates")
   return
